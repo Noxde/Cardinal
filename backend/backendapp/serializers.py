bb7a9ec27e1f885d371c39ac832a1caca66b362d
Valentino Amato <valentinoamatoroberts@gmail.com>
2023-01-19 13:54:04 -0300

    Created serializer for custom user model.

---
+++ backend/backendapp/serializers.py
@@ -0,0 +1,23 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer): #Serializer for the custom user model
+    class Meta:
+        model = get_user_model()
+        fields = ['id',
+                  'username', 
+                  'email',
+                  'first_name',
+                  'last_name',
+                  'about',
+                  'region',
+                  'lang',
+                  'birth_date',
+                  'date_joined',
+                  'last_login',
+                ]
+
+
+
