7705fa74f21eb806c5ca33bb31e1c73eb56c8691
Valentino Amato <valentinoamatoroberts@gmail.com>
2023-01-19 13:52:31 -0300

    Created custom user model.
    Added function in the User class that retrieves field names.

--- backend/backendapp/models.py
+++ backend/backendapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):     #Custom user model
+    about = models.TextField(max_length=1000,blank=True)
+    region = models.CharField(max_length=50,blank=True)
+    lang = models.CharField(max_length=50,blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    date_joined = models.DateField(null=True, blank=True)
+    last_login = models.DateField(null=True, blank=True)
+
+    def getfields():          #Returns the name of the fields as strings in a list
+        return [field.name for field in User._meta.get_fields()]
+    
