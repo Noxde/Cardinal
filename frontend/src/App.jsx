d3156acabc2001ed94691a7b606756e417029e42
Noxde <mateo.tesorieri@hotmail.com>
2023-01-24 20:51:37 -0300

    Added authprovider, 404 component and private routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,25 @@
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import NotFound from "./components/NotFound";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
+import PrivateRoutes from "./utils/PrivateRoutes";
+
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route element={<PrivateRoutes />}>
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<Login />} />
+          </Route>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
