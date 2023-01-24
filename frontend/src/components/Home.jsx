5cfdcb236294475612f19b75dc96a740522ecd9d
Noxde <mateo.tesorieri@hotmail.com>
2023-01-24 20:59:24 -0300

    Added links to all urls for development

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,27 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
 
 function Home() {
-  return <div>Home</div>;
+  let { user, logoutUser } = useContext(AuthContext);
+
+  return (
+    <div className="max-w-[200px] space-y-2">
+      {user && <h1>{user}</h1>}
+
+      <Link className="block btn" to="signup">
+        Sign up
+      </Link>
+      <Link className="block btn" to="signin">
+        Sign in
+      </Link>
+      <Link className="block btn" to="404">
+        404
+      </Link>
+
+      {user && <button onClick={logoutUser}>Log out</button>}
+    </div>
+  );
 }
 
 export default Home;
