071682f0efb776b92a9c465732ef0432783b4ed6
Valentino Amato <valentinoamatoroberts@gmail.com>
2023-01-27 19:57:05 -0300

    Created userauth/tokens.py.
    -Added class to create tokens for email confirmation.
    -Added a funcion to generate the simple_jwt token (previously found in views.py).

---
+++ backend/userauth/tokens.py
@@ -0,0 +1,20 @@
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+import six  
+
+def getjwtoken(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
+
+class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        return (
+            six.text_type(user.pk) + six.text_type(timestamp)  + six.text_type(user.is_active)
+        )
+
+account_activation_token = AccountActivationTokenGenerator()
+
