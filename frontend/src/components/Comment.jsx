08b41708afdbd2ea3535e13cda0c9c63d7c85391
Noxde <mateo.tesorieri@hotmail.com>
2023-01-31 21:56:41 -0300

    Added comment component(timestamp missing)

---
+++ frontend/src/components/Comment.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Comment({ author: { profilePicture, username }, content }) {
+  return (
+    <div className="flex items-start p-4">
+      <img
+        width={"50px"}
+        src={profilePicture}
+        alt=""
+        className="rounded-full mr-4"
+      />
+      <div>
+        <span className="Gelion-Medium">{username}</span>
+        <p>{content}</p>
+      </div>
+      <span className="timestamp ml-auto">15min</span>
+    </div>
+  );
+}
+
+export default Comment;
