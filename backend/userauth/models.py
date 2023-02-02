b25f037a76cd2e22a07abe5ed710c83a2370eb40
Valentino Amato <valentinoamatoroberts@gmail.com>
2023-02-02 19:08:43 -0300

    Added profile and banner picture fields to custom user model. Also added following field with recursive many to many relationship.

--- backend/userauth/models.py
+++ backend/userauth/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import AbstractUser, UserManager
 
 
 class User(AbstractUser):     #Custom user model
+    profileimg = models.ImageField(upload_to='user/',blank=True)
+    banner = models.ImageField(upload_to='user/',blank=True)
     email = models.EmailField(unique=True)
     about = models.TextField(max_length=1000,blank=True)
     region = models.CharField(max_length=50,blank=True)
@@ -11,11 +13,11 @@ class User(AbstractUser):     #Custom user model
     date_joined = models.DateField(null=True, blank=True)
     last_login = models.DateTimeField(null=True, blank=True)
     is_active = models.BooleanField(default=False)
+    following = models.ManyToManyField('self',symmetrical=False,blank=True)
 
-    
 
     def getfields():          #Returns the fields that can be modified with setattr()
         fields=['username','email','first_name','last_name','about','region','lang','birth_date']
         # return [field.name for field in User._meta.get_fields()]
         return fields
-    
+
