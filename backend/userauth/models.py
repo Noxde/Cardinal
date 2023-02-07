d28b9c3899d180ccf2f1facafa4623ec581d5699
Valentino Amato <valentinoamatoroberts@gmail.com>
2023-02-07 18:50:54 -0300

    Created model in userauth to log sent emails and avoid spam.

--- backend/userauth/models.py
+++ backend/userauth/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
-import os
+import os, datetime
 from .tokens import createToken
+from django.utils import timezone
+
+
 
 class User(AbstractUser):     #Custom user model
     profileimg = models.ImageField(upload_to='user/',blank=True)
@@ -29,5 +32,43 @@ class User(AbstractUser):     #Custom user model
     def getfields():          #Returns all the names of the fields in User
  
         return [field.name for field in User._meta.get_fields()]
-        
 
+
+class Emails(models.Model): #Keeps a log of all the emails sent to avoid spamming
+    EMAILCONFIRMATION = 'EC'
+    PASSWORDRESET = 'PR'
+    
+    emailsubjects = [
+    (EMAILCONFIRMATION, 'EmailConfirmation'),
+    (PASSWORDRESET, 'PasswordReset'),
+    ]
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+    subject = models.CharField(
+        max_length=2,
+        choices=emailsubjects,
+    )
+
+    def last_hour():
+        return timezone.now() - timezone.timedelta(hours=1)
+
+    def last_day():
+        return timezone.now() - timezone.timedelta(days=1)
+
+    def reset(user):
+        Emails.objects.filter(time__lt=Emails.last_day()).delete()
+    
+    def spam(user,subject):
+        HOURLY_LIMIT = 3
+        DAILY_LIMIT = 10
+
+        Emails.reset(user)
+
+        last_hour_emails = len(Emails.objects.filter(user=user,subject=subject,time__gt=Emails.last_hour()))
+        last_day_emails = len(Emails.objects.filter(user=user,subject=subject,time__gt=Emails.last_day()))
+
+        if last_hour_emails>HOURLY_LIMIT or last_day_emails>DAILY_LIMIT:
+            return True
+        else:
+            return False
+        
