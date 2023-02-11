efd5bd63fe571579698132b4a8654c2b08371012
Valentino Amato <valentinoamatoroberts@gmail.com>
2023-02-11 20:48:48 -0300

    Created view to create a new post.

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from . import views
 
 
 
 urlpatterns = [
+    path('createpost/', views.createpost.as_view(), name='createpost'),
 
     
 ]
--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Post, Files
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
-# Create your views here.
+
+
+class createpost(APIView): #Creates a new post 
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        content = request.data.get('content',False)
+        
+        files = []
+        for _ in range(10):
+            file = request.data.get(str(_),False)
+            if file:
+                files.append(file)
+        
+        if content:
+            try:
+                post = Post.objects.create(user=user,content=content)
+                if files:
+                    for file in files:
+                        print(file)
+                        Files.objects.create(post=post,file=file)
+
+                return JsonResponse({'status':'Post Created Successfully.'}, status=201)
+
+            except Exception as e:
+                print(e)
+                return JsonResponse({'status':'Failed to Create Post.'}, status=500)
+        
+        else:
+            return JsonResponse({'status':'Missing content.'}, status=400)
+
+
