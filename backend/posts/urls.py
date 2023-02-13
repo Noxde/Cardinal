171a0ec1726220d2a348485673c316a74e211475
Valentino Amato <valentinoamatoroberts@gmail.com>
2023-02-13 17:54:35 -0300

    Added view to add or remove likes from posts.

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('createpost/', views.createpost.as_view(), name='createpost'),
     path('delete/', views.delete.as_view(), name='delete'),
+    path('likes/<str:context>/<str:action>/<int:id>/', views.likes.as_view(), name='likes'),
 
     
 ]
--- backend/posts/views.py
+++ backend/posts/views.py
@@ -23,7 +23,6 @@ class createpost(APIView): #Creates a new post
                 post = Post.objects.create(user=user,content=content)
                 if files:
                     for file in files:
-                        print(file)
                         Files.objects.create(post=post,file=file)
 
                 return JsonResponse({'status':'Post Created Successfully.'}, status=201)
@@ -54,6 +53,34 @@ class delete(APIView): #Deletes a post
         
         else:
             return JsonResponse({'status':'Wrong or missing id.'}, status=400)
+
+
+class likes(APIView): #Adds and removes likes from posts  
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, context, action,id):
+        user = request.user
+        def postlikes():
+            try:
+                post = Post.objects.get(id=id)
+            except Exception:
+                return JsonResponse({'status':'Id does not match any post.'}, status=400)
+            if action == 'add':
+                post.likes.add(user)
+            elif action == 'remove':
+                post.likes.remove(user)
+            else:
+               return JsonResponse({'status':'Wrong action.'}, status=400)
+            
+            return JsonResponse({'status':'Operation Successful.'}, status=200)
+            
+        if context == 'post':
+            return postlikes()        
+        
+        else:
+            return JsonResponse({'status':'Wrong context.'}, status=400)
+
+
 
 
 
