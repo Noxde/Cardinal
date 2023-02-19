fb88311501dccee5ba1eb3e0a3ce467823b2016f
Noxde <mateo.tesorieri@hotmail.com>
2023-02-19 18:08:22 -0300

    Added email confirmation in case account isn't verified

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,5 @@
-import { useContext, useState } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Link, Navigate, useLocation } from "react-router-dom";
 
 import { useFormik } from "formik";
 
@@ -7,10 +7,22 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { LoginSchema } from "../schema/index.js";
 import AuthContext from "../context/AuthContext.js";
+import ConfirmEmail from "./ConfirmEmail.jsx";
+import LoginConfirmEmail from "./LoginConfirmEmail.jsx";
 
 function Login() {
-  let { loginUser } = useContext(AuthContext);
+  let { loginUser, user } = useContext(AuthContext);
   const [disableSign, setDisableSign] = useState(false);
+
+  const [confirmEmail, setConfirmEmail] = useState(false);
+  const [emailVerification, setEmailVerification] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state) {
+      formik.values.login = location.state.username;
+    }
+  });
 
   const formik = useFormik({
     initialValues: {
@@ -21,7 +33,6 @@ function Login() {
     validationSchema: LoginSchema,
     //Submit Form
     onSubmit: async (values) => {
-      console.log("Submitted");
       setDisableSign(true);
       toast.loading("Signing in...", {
         toastId: "signin",
@@ -40,7 +51,29 @@ function Login() {
           isLoading: false,
         });
       } catch (err) {
-        console.log(err);
+        if (err.response.status === 403) {
+          return toast.update("signin", {
+            type: "error",
+            isLoading: false,
+            render: (
+              <p>
+                User email address is not confirmed.{" "}
+                <button
+                  onClick={() => {
+                    toast.dismiss();
+                    setConfirmEmail(true);
+                  }}
+                  className="underline"
+                  type="button"
+                >
+                  Click here
+                </button>{" "}
+                to get a new confirmation email
+              </p>
+            ),
+          });
+        }
+
         toast.update("signin", {
           type: "error",
           isLoading: false,
@@ -48,116 +81,132 @@ function Login() {
           closeOnClick: true,
           closeButton: true,
           draggable: true,
-          render:
-            err.response?.data.status ||
-            "Something went wrong, try again later.",
+          render: "Something went wrong, try again later.",
           onClose: () => setDisableSign(false),
         });
       }
     },
   });
 
-  return (
-    <div className="min-h-screen grid place-items-center Gelion-Regular text-[#606161] py-8">
-      <div className="w-full px-4">
-        <div className="text-center space-y-4 mb-5">
-          <img
-            src={"/logo.svg"}
-            className="mx-auto"
-            width="50px"
-            alt="bird logo"
-          />
-          <h1 className="text-4xl Gelion-Semi-Bold text-[#131313]">Sign in</h1>
-          <p className="max-w-[22rem] mx-auto text-lg">
-            Welcome back! Enter your email/username and password below to sign
-            in.
-          </p>
-        </div>
+  return user ? (
+    <Navigate to={"/"} />
+  ) : emailVerification ? (
+    <ConfirmEmail formik={formik} />
+  ) : (
+    <>
+      {confirmEmail && (
+        <LoginConfirmEmail
+          formik={formik}
+          setEmailVerification={setEmailVerification}
+        />
+      )}
 
-        <form
-          onSubmit={formik.handleSubmit}
-          className="text-[#131313] max-w-lg mx-auto"
-        >
-          <ToastContainer theme="colored" />
-          <label className="block Gelion-Semi-Bold" htmlFor="login">
-            Email/Username
-          </label>
-          <input
-            className={`w-full rounded-lg border-[#9C9D9F] mt-2 ${
-              formik.touched.login && formik.errors.login ? "input-error" : ""
-            }`}
-            type="text"
-            name="login"
-            placeholder="Enter your user or email"
-            value={formik.values.login}
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-          />
-          {formik.touched.login && formik.errors.login && (
-            <p className="text-[#f14141]">{formik.errors.login}</p>
-          )}
-
-          <label className="block Gelion-Semi-Bold  mt-5" htmlFor="password">
-            Password
-          </label>
-          <input
-            className={`w-full rounded-lg border-[#9C9D9F] mt-2 ${
-              formik.touched.password && formik.errors.password
-                ? "input-error"
-                : ""
-            }`}
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            value={formik.values.password}
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-          />
-          {formik.touched.password && formik.errors.password && (
-            <p className="text-[#f14141]">{formik.errors.password}</p>
-          )}
-
-          <div className="flex justify-between items-center mt-2">
-            <div className="flex items-center">
-              <input
-                className="rounded-md"
-                type="checkbox"
-                name="keepLogged"
-                id="keepLogged"
-                onChange={formik.handleChange}
-              />
-              <label
-                className="Gelion-Semi-Bold text-sm ml-2"
-                htmlFor="keepLogged"
-              >
-                Keep me logged in
-              </label>
-            </div>
-
-            <Link
-              className="text-sm Gelion-Semi-Bold text-[#606161]"
-              to={"/reset-password"}
-            >
-              Forgot password
-            </Link>
+      <div className="min-h-screen grid place-items-center Gelion-Regular text-[#606161] py-8">
+        <div className="w-full px-4">
+          <div className="text-center space-y-4 mb-5">
+            <img
+              src={"/logo.svg"}
+              className="mx-auto"
+              width="50px"
+              alt="bird logo"
+            />
+            <h1 className="text-4xl Gelion-Semi-Bold text-[#131313]">
+              Sign in
+            </h1>
+            <p className="max-w-[22rem] mx-auto text-lg">
+              Welcome back! Enter your email/username and password below to sign
+              in.
+            </p>
           </div>
 
-          <input
-            className="enabled:bg-[#4558ff] text-white w-full mt-5 rounded-lg py-3 Gelion-Semi-Bold cursor-pointer enabled:hover:brightness-90 enabled:active:outline enabled:active:outline-offset-4 enabled:active:outline-4 enabled:active:outline-[#4558ff] disabled:bg-[hsl(234,100%,20%)] disabled:cursor-not-allowed"
-            type="submit"
-            disabled={disableSign}
-            value="Sign in"
-          />
-        </form>
+          <form
+            onSubmit={formik.handleSubmit}
+            className="text-[#131313] max-w-lg mx-auto"
+          >
+            <ToastContainer theme="colored" />
+            <label className="block Gelion-Semi-Bold" htmlFor="login">
+              Email/Username
+            </label>
+            <input
+              className={`w-full rounded-lg border-[#9C9D9F] mt-2 ${
+                formik.touched.login && formik.errors.login ? "input-error" : ""
+              }`}
+              type="text"
+              name="login"
+              placeholder="Enter your user or email"
+              value={formik.values.login}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+            />
+            {formik.touched.login && formik.errors.login && (
+              <p className="text-[#f14141]">{formik.errors.login}</p>
+            )}
 
-        <p className="text-center mt-5">
-          Don't have an account?
-          <Link className="ml-3 Gelion-Semi-Bold text-[#131313]" to="/register">
-            Sign up
-          </Link>
-        </p>
+            <label className="block Gelion-Semi-Bold  mt-5" htmlFor="password">
+              Password
+            </label>
+            <input
+              className={`w-full rounded-lg border-[#9C9D9F] mt-2 ${
+                formik.touched.password && formik.errors.password
+                  ? "input-error"
+                  : ""
+              }`}
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              value={formik.values.password}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+            />
+            {formik.touched.password && formik.errors.password && (
+              <p className="text-[#f14141]">{formik.errors.password}</p>
+            )}
+
+            <div className="flex justify-between items-center mt-2">
+              <div className="flex items-center">
+                <input
+                  className="rounded-md"
+                  type="checkbox"
+                  name="keepLogged"
+                  id="keepLogged"
+                  onChange={formik.handleChange}
+                />
+                <label
+                  className="Gelion-Semi-Bold text-sm ml-2"
+                  htmlFor="keepLogged"
+                >
+                  Keep me logged in
+                </label>
+              </div>
+
+              <Link
+                className="text-sm Gelion-Semi-Bold text-[#606161]"
+                to={"/reset-password"}
+              >
+                Forgot password
+              </Link>
+            </div>
+
+            <input
+              className="enabled:bg-[#4558ff] text-white w-full mt-5 rounded-full py-3 Gelion-Semi-Bold cursor-pointer enabled:hover:brightness-90 enabled:active:outline enabled:active:outline-offset-4 enabled:active:outline-4 enabled:active:outline-[#4558ff] disabled:bg-[hsl(234,100%,20%)] disabled:cursor-not-allowed"
+              type="submit"
+              disabled={disableSign}
+              value="Sign in"
+            />
+          </form>
+
+          <p className="text-center mt-5">
+            Don't have an account?
+            <Link
+              className="ml-3 Gelion-Semi-Bold text-[#131313]"
+              to="/register"
+            >
+              Sign up
+            </Link>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
