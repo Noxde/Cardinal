99f5d9c7b49c65e7ee9d126dcf22e64c3b71b252
Noxde <mateo.tesorieri@hotmail.com>
2023-02-25 17:01:23 -0300

    Removed the need for multiple states and fixes

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,41 +1,65 @@
-import { useContext, useState } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Link, Navigate, useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 import instance from "../utils/Axios";
+import useAxios from "../utils/useAxios";
 import AuthContext from "../context/AuthContext";
 
 import { AiFillHome } from "react-icons/ai";
 import { GoSearch } from "react-icons/go";
 import { TfiEmail, TfiPencil } from "react-icons/tfi";
 
+import NotFound from "./NotFound";
 import Loading from "./Loading";
 import Post from "./Post";
 
 import Posts from "../utils/posts";
 
 function Profile() {
-  let { getUserInfo, modUserInfo } = useContext(AuthContext);
+  const { user, setUser } = useContext(AuthContext);
+  const [userProfile, setUserProfile] = useState(null);
+  const isLoggedProfile = user?.username !== userProfile?.username;
+
   const [posts, setPosts] = useState(Posts); // No posts from the backend yet
   const [isFollowed, setIsFollowed] = useState(false);
-  const [about, setAbout] = useState("");
-  const [username, setUsername] = useState("");
-  const [banner, setBanner] = useState("");
-  const [profileimg, setProfileimg] = useState(
-    "/assets/profile_placeholder.png"
+
+  const { username } = useParams();
+  const api = useAxios();
+
+  useEffect(() => {
+    if (user && !username) {
+      setUserProfile(user);
+    }
+  }, []);
+  useEffect(() => {
+    let oldTitle = document.title;
+
+    document.title = `${userProfile?.username}'s Profile`;
+
+    return () => {
+      document.title = oldTitle;
+    };
+  }, [userProfile]);
+
+  console.log(userProfile);
+
+  const { isLoading, isError, error } = useQuery(
+    "userProfile",
+    () => instance.get(`/getpublicprofile/${username}/`),
+    {
+      enabled: !username && user ? false : true,
+      retry: false,
+      refetchOnWindowFocus: false,
+      cacheTime: 1000,
+      onSuccess: ({ data: userInfo }) => {
+        setUserProfile(userInfo);
+      },
+    }
   );
 
-  const { isLoading, isError, error } = useQuery("userProfile", getUserInfo, {
-    retry: false,
-    refetchOnWindowFocus: false,
-    onSuccess: (userInfo) => {
-      setProfileimg((prev) =>
-        userInfo?.profileimg ? `${userInfo?.profileimg}` : prev
-      );
-      setAbout(userInfo?.about);
-      setUsername(userInfo?.username);
-      setBanner(userInfo?.banner);
-    },
-  });
+  if (user === null && !username) {
+    return <Navigate to={"/login"} />;
+  }
 
   if (isLoading) {
     return <Loading />;
@@ -43,88 +67,41 @@ function Profile() {
   if (isError) {
     // Probably expired token, we delete it and send user to login again
     console.error(error.message);
-    localStorage.removeItem("authTokens");
-    window.location.pathname = "/login";
-    return <Loading />; // Avoid empty profile showing up
+    return <NotFound />; // Avoid empty profile showing up
   }
   let iconSize = "30px";
 
   return (
-    <div className="lg:grid grid-cols-[1.5fr,5fr,2.5fr] lg:max-w-[1900px] lg:mx-auto">
-      <div className="hidden lg:flex justify-center h-full bg-white border-r border-[#e6e6e6] z-10">
-        <div>
-          <div className="sticky top-10 h-40">
-            <img src="/logo.svg" width={"70px"} />
-            <nav className="text-2xl Gelion-Medium mt-10">
-              <ul className="space-y-4">
-                <li>
-                  <Link to={"/"}>Home</Link>
-                </li>
-                <li>
-                  <Link>Messages</Link>
-                </li>
-                <li>
-                  <Link>Profile</Link>
-                </li>
-                <li>
-                  <Link>More</Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col bg-[#1d1d1d] min-h-screen text-[#3C3D44]">
-        {/* banner image */}
-        <label
-          htmlFor="uploadBanner"
-          className="relative w-full inset-0 cursor-pointer -mb-6"
-        >
-          <div className="absolute grid place-items-center inset-0 opacity-0 hover:opacity-100 bg-[rgba(0,0,0,0.5)]">
-            <TfiPencil color="#c1c1c1" size={"25px"} />
-          </div>
-          <img
-            src={banner}
-            alt=""
-            className="h-64 w-full max-w-none object-cover"
-          />
-        </label>
-
-        <input
-          type="file"
-          id="uploadBanner"
-          accept="image/*"
-          className="hidden"
-          onChange={async (e) => {
-            const file = e.target.files[0];
-            const reader = new FileReader();
-            /**TODO:
-             * Handle error, move to authcontext
-             *  */
-            reader.readAsDataURL(file);
-            await modUserInfo({ banner: file });
-            window.location.reload();
-          }}
+    <div className="flex flex-col bg-[#1d1d1d] min-h-screen text-[#3C3D44]">
+      {/* banner image */}
+      {isLoggedProfile ? (
+        <img
+          src={userProfile?.banner}
+          alt=""
+          className="h-64 w-full max-w-none object-cover -mb-6"
         />
-
-        {/*  */}
-        <div className="relative flex-1 bg-white rounded-t-[1.7rem] lg:rounded-none shadow-[0px_-50px_70px_0px_rgba(0,0,0,0.5)]">
-          <label htmlFor="uploadProfile">
-            <div className="grid place-items-center absolute w-[92px] cursor-pointer object-cover left-1/2 aspect-square -translate-x-1/2 -top-11 opacity-0 bg-[rgba(0,0,0,0.5)] hover:opacity-100 rounded-full z-40">
+      ) : (
+        /**TODO:
+         * Move upload image to profile settings
+         */
+        <>
+          <label
+            htmlFor="uploadBanner"
+            className="relative w-full inset-0 cursor-pointer -mb-6"
+          >
+            <div className="absolute grid place-items-center inset-0 opacity-0 hover:opacity-100 bg-[rgba(0,0,0,0.5)]">
               <TfiPencil color="#c1c1c1" size={"25px"} />
             </div>
             <img
-              src={profileimg}
-              width={"100px"}
-              alt="Profile"
-              className={`absolute cursor-pointer object-cover left-1/2 aspect-square -translate-x-1/2 -top-12 bg-gray-200 border-4 border-white rounded-full
-            `}
+              src={userProfile?.banner}
+              alt=""
+              className="h-64 w-full max-w-none object-cover"
             />
           </label>
 
           <input
             type="file"
-            id="uploadProfile"
+            id="uploadBanner"
             accept="image/*"
             className="hidden"
             onChange={async (e) => {
@@ -132,98 +109,208 @@ function Profile() {
               const reader = new FileReader();
               /**TODO:
                * Handle error, move to authcontext
-               *  */
+               */
+
               reader.readAsDataURL(file);
-              await modUserInfo({ profileimg: file });
-              window.location.reload();
+              reader.onload = () => {
+                setUserProfile({ ...userProfile, banner: reader.result });
+                setUser({ ...userProfile, banner: reader.result });
+                localStorage.setItem(
+                  "user",
+                  JSON.stringify({
+                    ...userProfile,
+                    banner: reader.result,
+                  })
+                );
+              };
+              await api.post(
+                "/moduserinfo/",
+                {
+                  banner: file,
+                },
+                {
+                  headers: {
+                    "Content-Type": "multipart/form-data",
+                  },
+                }
+              );
             }}
           />
+        </>
+      )}
 
-          <div className="pt-14 px-4 space-y-3">
-            {/* username and follow button */}
-            <div className="flex justify-between items-start">
-              <span id="username" className="text-xl Gelion-Medium">
-                {username}
-              </span>
+      {/* Profile picture */}
+      <div className="relative flex-1 bg-white rounded-t-[1.7rem] lg:rounded-none shadow-[0px_-50px_70px_0px_rgba(0,0,0,0.5)]">
+        {isLoggedProfile ? (
+          <img
+            src={userProfile?.profileimg || "/assets/profile_placeholder.png"}
+            width={"100px"}
+            alt="Profile"
+            className={`absolute object-cover left-1/2 aspect-square -translate-x-1/2 -top-12 bg-gray-200 border-4 border-white rounded-full
+  `}
+          />
+        ) : (
+          /**TODO:
+           * Move upload image to profile settings
+           */
+          <>
+            <label htmlFor="uploadProfile" className="cursor-pointer">
+              <div className="grid place-items-center absolute w-[92px] cursor-pointer object-cover left-1/2 aspect-square -translate-x-1/2 -top-11 opacity-0 bg-[rgba(0,0,0,0.5)] hover:opacity-100 rounded-full z-40">
+                <TfiPencil color="#c1c1c1" size={"25px"} />
+              </div>
+              <img
+                src={
+                  userProfile?.profileimg || "/assets/profile_placeholder.png"
+                }
+                width={"100px"}
+                alt="Profile"
+                className={`absolute object-cover left-1/2 aspect-square -translate-x-1/2 -top-12 bg-gray-200 border-4 border-white rounded-full
+            `}
+              />
+            </label>
+
+            <input
+              type="file"
+              id="uploadProfile"
+              accept="image/*"
+              className="hidden"
+              onChange={async (e) => {
+                const file = e.target.files[0];
+                const reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.onload = () => {
+                  setUserProfile({ ...userProfile, profileimg: reader.result });
+                  setUser({ ...userProfile, profileimg: reader.result });
+                  localStorage.setItem(
+                    "user",
+                    JSON.stringify({
+                      ...userProfile,
+                      profileimg: reader.result,
+                    })
+                  );
+                };
+                await api.post(
+                  "/moduserinfo/",
+                  {
+                    profileimg: file,
+                  },
+                  {
+                    headers: {
+                      "Content-Type": "multipart/form-data",
+                    },
+                  }
+                );
+              }}
+            />
+          </>
+        )}
+
+        <div className="pt-14 px-4 space-y-3">
+          {/* username and follow button */}
+          <div className="flex justify-between items-start">
+            <span id="username" className="text-xl Gelion-Medium">
+              {userProfile?.username}
+            </span>
+            {isLoggedProfile ? (
               <button
                 onClick={() => setIsFollowed((prev) => !prev)}
                 className="Gelion-Medium bg-[#4558ff] text-white px-6 py-1 rounded-full"
               >
                 {isFollowed ? "Following" : "Follow"}
               </button>
-            </div>
+            ) : (
+              <button
+                onClick={() => {
+                  console.log("Edit profile");
+                }}
+                className="Gelion-Medium bg-[#4558ff] text-white px-6 py-1 rounded-full"
+              >
+                Edit profile
+              </button>
+            )}
+          </div>
 
-            <div className="info flex space-x-6">
-              <div className="flex flex-col text-center justify-center">
-                <span className="Gelion-Medium">{posts.length}</span>
-                Posts
-              </div>
-              <div className="followers flex flex-col text-center justify-center">
-                <span className="Gelion-Medium">{isFollowed ? 1 : 0}</span>
-                Followers
-              </div>
-              <div className="following flex flex-col text-center justify-center">
-                <span className="Gelion-Medium">0</span>
-                Following
-              </div>
+          <div className="info flex space-x-6">
+            <div className="flex flex-col text-center justify-center">
+              <span className="Gelion-Medium">{posts.length}</span>
+              Posts
             </div>
-            <p className="about">{about}</p>
+            <div className="followers flex flex-col text-center justify-center">
+              <span className="Gelion-Medium">
+                {userProfile?.followers.length}
+              </span>
+              Followers
+            </div>
+            <div className="following flex flex-col text-center justify-center">
+              <span className="Gelion-Medium">
+                {userProfile?.following.length}
+              </span>
+              Following
+            </div>
           </div>
-          <div className="selector flex justify-evenly my-4 py-4 border-y border-[#e6e6e6]">
-            <button className="text-[#4558ff] Gelion-Medium">Posts</button>
-            <button disabled className="opacity-80">
-              Posts and replies
-            </button>
-            <button disabled className="opacity-80">
-              Media
-            </button>
-            <button disabled className="opacity-80">
-              Like
-            </button>
-          </div>
-          {/* all posts go here */}
-          {posts.length ? (
-            <div className="posts pb-28">
-              {posts.map((post) => (
-                <Post
-                  key={Math.random()}
-                  author={post.author}
-                  content={post.content}
-                  attachment={post.attachment}
-                />
-              ))}
-            </div>
-          ) : (
-            <div className="text-center bg-white Gelion-Medium">
-              <p>There aren't any posts to show here</p>
-            </div>
-          )}
+          <p className="about">{userProfile?.about}</p>
         </div>
-        {/* bottom nav */}
-        <div className="lg:hidden flex py-2 px-1 items-center justify-around fixed bottom-4 left-1/2 -translate-x-1/2 rounded-full w-[90%] bg-white shadow-[0px_4px_30px_-5px_rgba(0,0,0,0.2)]">
-          <Link to="/">
-            <AiFillHome size={iconSize} color="#3e4fe5" />
-          </Link>
-          <button>
-            <GoSearch size={iconSize} color="#908f94" />
+        <div className="selector flex justify-evenly my-4 py-4 border-y border-[#e6e6e6]">
+          <button className="text-[#4558ff] Gelion-Medium">Posts</button>
+          <button disabled className="opacity-80">
+            Posts and replies
           </button>
-          <button className="flex items-center justify-center relative rounded-full -mt-6 -top-3 bg-[#3e4fe5] w-[70px] aspect-[1/1]">
-            <TfiPencil size={iconSize} color="white" />
+          <button disabled className="opacity-80">
+            Media
           </button>
-          <button>
-            <TfiEmail size={iconSize} color="#908f94" />
-          </button>
-          <button>
-            <img
-              src={profileimg}
-              width={iconSize}
-              alt="profile placeholder"
-              className="rounded-full aspect-square"
-            />
+          <button disabled className="opacity-80">
+            Like
           </button>
         </div>
+        {/* all posts go here */}
+        {posts.length ? (
+          <div className="posts pb-28">
+            {posts.map((post) => (
+              <Post
+                key={Math.random()}
+                author={post.author}
+                content={post.content}
+                attachment={post.attachment}
+              />
+            ))}
+          </div>
+        ) : (
+          <div className="text-center bg-white Gelion-Medium">
+            <p>There aren't any posts to show here</p>
+          </div>
+        )}
       </div>
-      <div className="hidden lg:block h-full bg-white border-l border-[#e6e6e6] z-30"></div>
+      {/* bottom nav */
+      /**TODO:
+       * Move out of profile component
+       */}
+
+      <div className="lg:hidden flex py-2 px-1 items-center justify-around fixed bottom-4 left-1/2 -translate-x-1/2 rounded-full w-[90%] bg-white shadow-[0px_4px_30px_-5px_rgba(0,0,0,0.2)]">
+        <Link to="/">
+          <AiFillHome size={iconSize} color="#3e4fe5" />
+        </Link>
+        <button>
+          <GoSearch size={iconSize} color="#908f94" />
+        </button>
+        <button className="flex items-center justify-center relative rounded-full -mt-6 -top-3 bg-[#3e4fe5] w-[70px] aspect-[1/1]">
+          <TfiPencil size={iconSize} color="white" />
+        </button>
+        <button>
+          <TfiEmail size={iconSize} color="#908f94" />
+        </button>
+        <button>
+          <img
+            src={
+              user
+                ? user.profileimg || "/assets/profile_placeholder.png"
+                : "/assets/profile_placeholder.png"
+            }
+            width={iconSize}
+            alt="profile placeholder"
+            className="rounded-full aspect-square"
+          />
+        </button>
+      </div>
     </div>
   );
 }
