a131f9fbd033f1eac87c1e6864b39c593d97e4e0
Noxde <mateo.tesorieri@hotmail.com>
2023-02-25 16:53:20 -0300

    Added layout component for some routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  BrowserRouter as Router,
+  Navigate,
+} from "react-router-dom";
 import NotFound from "./components/NotFound";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-import PrivateRoutes from "./utils/PrivateRoutes";
+// import PrivateRoutes from "./utils/PrivateRoutes";
 import ConfirmedEmail from "./components/ConfirmedEmail";
 import { QueryClient, QueryClientProvider } from "react-query";
+import Layout from "./components/Layout";
 
 import { AuthProvider } from "./context/AuthContext";
+import jwtDecode from "jwt-decode";
 
 const queryClient = new QueryClient();
 
@@ -18,14 +25,17 @@ function App() {
       <AuthProvider>
         <QueryClientProvider client={queryClient}>
           <Routes>
-            <Route path="/" element={<Home />} />
             <Route path="*" element={<NotFound />} />
             <Route path="/confirmed-email/:user" element={<ConfirmedEmail />} />
             <Route path="/register" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-            {/* <Route element={<PrivateRoutes />}></Route> */}
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/profile/" element={<Profile />} />
+              <Route path="/profile/:username" element={<Profile />} />
+            </Route>
           </Routes>
+          {/* <Route element={<PrivateRoutes />}></Route> */}
         </QueryClientProvider>
       </AuthProvider>
     </Router>
