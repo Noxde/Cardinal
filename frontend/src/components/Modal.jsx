3a7de817eeeeaed5560928ce11df2bcc6c7e8681
Noxde <mateo.tesorieri@hotmail.com>
2023-06-26 16:04:42 -0300

    Added modal component with support for ESC and clicking outside to close

---
+++ frontend/src/components/Modal.jsx
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+import { BiLeftArrowAlt } from "react-icons/bi";
+
+function Modal({ children, label, confirmLabel, confirmClick, setIsOpen }) {
+  useEffect(() => {
+    function escape(e) {
+      if (e.key == "Escape") setIsOpen(false);
+    }
+    function click(e) {
+      if (e.target.classList.contains("z-50")) {
+        setIsOpen(false);
+      }
+    }
+    document.addEventListener("keydown", escape);
+    document.addEventListener("click", click);
+
+    return () => {
+      document.removeEventListener("keydown", escape);
+      document.removeEventListener("click", click);
+    };
+  }, []);
+
+  return (
+    <div className="fixed inset-0 sm:bg-[#0d162acd] bg-white z-50 p-4">
+      <div className="absolute w-full sm:w-auto left-1/2 -translate-x-1/2 sm:top-1/2 sm:-translate-y-1/2 bg-white rounded-xl overflow-y-scroll">
+        {/* Header */}
+        <div className="sticky top-0 flex items-center text-xl Gelion-Medium border-b bg-white text-[#3c3d44] p-4">
+          <button
+            onClick={() => {
+              document.body.style.overflow = "auto";
+              setIsOpen(false);
+            }}
+          >
+            <BiLeftArrowAlt size={"30px"} color="#3c3d44" />
+          </button>
+          {label}
+          {confirmLabel && (
+            <button onClick={confirmClick} className="ml-auto mr-2">
+              {confirmLabel}
+            </button>
+          )}
+        </div>
+        {/* Content */}
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
