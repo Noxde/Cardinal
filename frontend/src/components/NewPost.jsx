50a2d8f95985b18adccc72b1cdb728078aa81136
Noxde <mateo.tesorieri@hotmail.com>
2023-06-27 23:43:12 -0300

    Now uses modal component and has named functions

--- frontend/src/components/NewPost.jsx
+++ frontend/src/components/NewPost.jsx
@@ -3,6 +3,8 @@ import { BiLeftArrowAlt } from "react-icons/bi";
 import { BsImageFill } from "react-icons/bs";
 import AuthContext from "../context/AuthContext";
 import useAxios from "../utils/useAxios";
+import Modal from "./Modal";
+import Button from "../components/Button";
 
 function NewPost({ setNewPost }) {
   const api = useAxios();
@@ -24,86 +26,77 @@ function NewPost({ setNewPost }) {
     window.location.reload();
   }
 
-  return (
-    <div className="fixed inset-0 sm:bg-[#0d162acd] bg-white z-50 p-4">
-      <div className="absolute w-full sm:w-auto left-1/2 -translate-x-1/2 sm:top-1/2 sm:-translate-y-1/2 bg-white rounded-xl max-h-[520px] overflow-y-scroll">
-        {/* Header */}
-        <div className="sticky top-0 flex items-center text-xl Gelion-Medium border-b bg-white text-[#3c3d44] p-4 z-50">
-          <button
-            onClick={() => {
-              document.body.style.overflow = "auto";
-              setNewPost(false);
-            }}
-          >
-            <BiLeftArrowAlt size={"30px"} color="#3c3d44" />
-          </button>
-        </div>
-        {/* Content */}
-        <div className="flex flex-col p-4 ">
-          <div className="flex items-start">
-            <img
-              className="rounded-full object-cover aspect-square mr-4"
-              width={"50px"}
-              src={user.profileimg || "/assets/profile_placeholder.png"}
-              alt=""
-            />
-            <textarea
-              className="w-full rounded-lg border-none focus:ring-0 resize-none"
-              placeholder="What's happening?"
-              maxLength="200"
-              onInput={(e) => {
-                e.target.style.height = "";
-                e.target.style.height = e.target.scrollHeight + 3 + "px";
+  function handleTextArea(e) {
+    e.target.style.height = "";
+    e.target.style.height = e.target.scrollHeight + 3 + "px";
 
-                setPost({
-                  ...post,
-                  content: e.target.value,
-                });
-              }}
-              rows={1}
-              cols={35}
+    setPost({
+      ...post,
+      content: e.target.value,
+    });
+  }
+
+  function handleImagePreview(e) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      setimgPreview(reader.result);
+      setPost({ ...post, 0: file });
+    };
+  }
+
+  return (
+    <Modal setIsOpen={setNewPost}>
+      <div className="flex flex-col p-4 ">
+        <div className="flex items-start">
+          <img
+            className="rounded-full object-cover aspect-square mr-4"
+            width={"50px"}
+            src={user.profileimg || "/assets/profile_placeholder.png"}
+            alt=""
+          />
+          <textarea
+            className="w-full rounded-lg border-none focus:ring-0 resize-none"
+            placeholder="What's happening?"
+            maxLength="200"
+            onInput={handleTextArea}
+            rows={1}
+            cols={35}
+          />
+        </div>
+        {imgPreview && (
+          <div className="max-h-[700px] w-fit max-w-[500px] rounded-xl overflow-hidden ml-[66px] mt-4">
+            <img
+              src={imgPreview}
+              alt="post preview"
+              className="object-cover object-center"
             />
           </div>
-          {imgPreview && (
-            <div className="max-h-[700px] w-fit max-w-[500px] rounded-xl overflow-hidden ml-[66px] mt-4">
-              <img
-                src={imgPreview}
-                alt="post preview"
-                className="object-cover object-center"
-              />
-            </div>
-          )}
-          <label
-            className="w-fit ml-[66px] mt-2 cursor-pointer"
-            htmlFor="uploadProfile"
-          >
-            <BsImageFill color="#4558ff" />
-            <input
-              type="file"
-              id="uploadProfile"
-              accept="image/*"
-              className="hidden"
-              onChange={async (e) => {
-                const file = e.target.files[0];
-                const reader = new FileReader();
-                reader.readAsDataURL(file);
-                reader.onload = () => {
-                  setimgPreview(reader.result);
-                  setPost({ ...post, 0: file });
-                };
-              }}
-            />
-          </label>
-          <button
-            className="ml-auto mt-2 bg-[#4558ff] text-white Gelion-Medium rounded-full px-4 py-1"
-            onClick={handlePost}
-            disabled={post?.content ? false : true}
-          >
-            Post
-          </button>
-        </div>
+        )}
+        <label
+          className="w-fit ml-[66px] mt-2 cursor-pointer"
+          htmlFor="uploadProfile"
+        >
+          <BsImageFill color="#4558ff" />
+          <input
+            type="file"
+            id="uploadProfile"
+            accept="image/*"
+            className="hidden"
+            onChange={handleImagePreview}
+          />
+        </label>
+        <button
+          className="ml-auto mt-2 bg-[#4558ff] text-white Gelion-Medium rounded-full px-4 py-1"
+          onClick={handlePost}
+          disabled={post?.content ? false : true}
+        >
+          Post
+        </button>
+        {/* <Button onClick={handlePost} value="Post" /> */}
       </div>
-    </div>
+    </Modal>
   );
 }
 
