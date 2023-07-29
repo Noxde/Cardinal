3f5cb949dcc03c0642014372dc481d72b72835d2
Noxde <mateo.tesorieri@hotmail.com>
2023-07-28 21:30:12 -0300

    Removed unused code

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -6,8 +6,6 @@ import AuthContext from "../../context/AuthContext";
 
 import Post from "../../components/Post";
 
-// import Posts from "../utils/posts";
-
 function Home() {
   const api = useAxios();
   const { user, setUser, logoutUser } = useContext(AuthContext);
@@ -53,27 +51,8 @@ function Home() {
 
   return (
     <div className="flex flex-col bg-[#1d1d1d] min-h-screen text-[#3C3D44]">
-      {/* banner image */}
-
-      {/* <div className="bg-cyan-400 h-64 -mb-6 lg:mb-0">
-        <button onClick={logoutUser}>Logout</button>
-      </div> */}
-
       {/* Profile picture */}
       <div className="relative flex-1 bg-white rounded-t-[1.7rem] lg:rounded-none ">
-        {/* shadow-[0px_-50px_70px_0px_rgba(0,0,0,0.5)] */}
-        {/* <div className="selector flex justify-evenly my-4 py-4 border-y border-[#e6e6e6]">
-          <button className="text-[#4558ff] Gelion-Medium">Posts</button>
-          <button disabled className="opacity-80">
-            Posts and replies
-          </button>
-          <button disabled className="opacity-80">
-            Media
-          </button>
-          <button disabled className="opacity-80">
-            Like
-          </button>
-        </div> */}
         {/* all posts go here */}
         {posts.length && user ? (
           <div className="posts mb-28">
