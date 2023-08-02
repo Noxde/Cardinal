651e7a2f7d044d33c5edcbef8a0cf57c02442ecc
Valentino Amato <valentinoamatoroberts@gmail.com>
2023-08-02 20:25:32 -0300

    Add model for chat messages.

--- backend/chats/models.py
+++ backend/chats/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Message(models.Model): #Model for chat messages
+
+    content = models.TextField(max_length=300)
+    sender = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,related_name="sender")
+    receiver = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,related_name="receiver")
+    creation_time = models.DateTimeField(auto_now_add=True)
