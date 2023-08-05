1496e5d1c8ade65cf372dc09f5f2b340c18e7473
Valentino Amato <valentinoamatoroberts@gmail.com>
2023-08-05 14:37:58 -0300

    Add endpoint to get open chats.

--- backend/chats/views.py
+++ backend/chats/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from .models import Message, Chat
-from .serializers import MessageSerializer
+from .serializers import MessageSerializer, ChatSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from django.db.models import Q,F
@@ -57,3 +57,31 @@ class createchat(APIView): #Creates a Chat object
                 return JsonResponse({'status':'MultipleObjectsReturned, Chat creation avoided.'},status=200)
             
         return JsonResponse({'status':'Chat already exists.'},status=200)
+    
+class getopenchats(APIView): #Gets all the open chats of an user
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        loguser = request.user
+
+        chats = Chat.objects.filter(Q(user_one=loguser) | Q(user_two=loguser))
+
+        if not chats:
+            return JsonResponse({'status':f'User "{loguser.username}" does not have any open chat.'},status=404)
+        
+        chats_serialized = []
+        for chat in chats:
+            chats_serialized.append(ChatSerializer(chat,context={'loguser':loguser}).data)
+        print(chats_serialized)
+        response = [chats_serialized.pop(0)]
+        for chat in chats_serialized:
+            i=0
+            last_message = chat['last_message']
+            if last_message==False:
+                Chat.objects.get(id=chat['id']).delete()
+                continue
+            while i<len(response)  and last_message['id']<response[i]['last_message']['id']:
+                i+=1
+            response.insert(i,chat)
+
+        return JsonResponse(response,safe=False,status=200)
