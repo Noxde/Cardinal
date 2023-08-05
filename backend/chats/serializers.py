0cc4d5aec50baab5deb210d083af984eb8b7d8f8
Valentino Amato <valentinoamatoroberts@gmail.com>
2023-08-05 14:31:43 -0300

    Create serializer for open chats.
    The serializers receives a chat as a parameter and returns:
     -Id of the chat.
     -Last message of the chat (serialized).
     -Username and profile image of one of the users of the chat.

--- backend/chats/serializers.py
+++ backend/chats/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
-from .models import Message
+from backend.settings import BACKEND_DOMAIN,MEDIA_URL 
+from .models import Message, Chat
+from django.db.models import Q, F
+
 
 class MessageSerializer(serializers.ModelSerializer): #Serializer for the Message model
 
@@ -11,3 +14,38 @@ class MessageSerializer(serializers.ModelSerializer): #Serializer for the Messag
                   'receiver',
                   'creation_time',
                  ]
+        
+class ChatSerializer(serializers.ModelSerializer): #Serializer for open chats
+    chat_user = serializers.SerializerMethodField()
+    last_message = serializers.SerializerMethodField()
+
+    def get_chat_user(self, chat):
+        loguser = self.context.get("loguser")
+        if loguser==chat.user_one:
+            chat_user=chat.user_two
+        else:
+            chat_user=chat.user_one
+
+        profileimgurl = BACKEND_DOMAIN+MEDIA_URL+chat_user.profileimg.name if chat_user.profileimg else '' 
+        
+        return {'id':chat_user.id,
+                'username':chat_user.username,
+                'profileimg':profileimgurl,
+               }
+
+    def get_last_message(self, chat):
+        try:
+            last_message = Message.objects.filter(Q(sender=chat.user_one) | Q(sender=chat.user_two))
+            last_message = last_message.filter(Q(receiver=chat.user_one) | Q(receiver=chat.user_two))
+            last_message = last_message.exclude(sender=F("receiver")).order_by('-id')[0]
+        except IndexError:
+            return False
+        return MessageSerializer(last_message).data
+
+    class Meta:
+        model = Chat
+        fields = ['id',
+                  'chat_user',
+                  'last_message',
+                  ]
+
