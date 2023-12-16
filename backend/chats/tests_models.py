67aabc42141b768279a3d089b90495ed0d36d7c0
Valentino Amato <valentinoamatoroberts@gmail.com>
2023-12-15 23:41:01 -0300

    Add unit test for Message model.

--- backend/chats/tests_models.py
+++ backend/chats/tests_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from .models import Chat
+from .models import Chat, Message
+from datetime import datetime,timezone,timedelta
 
 class ChatTestCase(TestCase):
     @classmethod
@@ -17,3 +18,29 @@ class ChatTestCase(TestCase):
     def test_Chat_user_two(self):
         """Chat.user_two is OK."""
         self.assertEqual(self.user2,self.chat.user_two)
+
+
+class MessageTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user1 = get_user_model().objects.create(username="Tyler",email="tyler@cardinal.com",password="Tyler123")
+        cls.user2 = get_user_model().objects.create(username="Larry",email="larry@cardinal.com",password="Larry123")
+        cls.message = Message.objects.create(content="Hello, Larry!!",sender=cls.user1,receiver=cls.user2) 
+    
+    def test_Message_content(self):
+        """Message content is correct."""
+        self.assertEqual(self.message.content,"Hello, Larry!!")
+
+    def test_Message_sender(self):
+        """Message sender is correct."""
+        self.assertEqual(self.message.sender,self.user1)
+    
+    def test_Message_receiver(self):
+        """Message receiver is correct."""
+        self.assertEqual(self.message.receiver,self.user2)
+
+    def test_Message_creation_time(self):
+        """Message creation_time is correct."""
+        message = Message.objects.create(content="I am speed.",sender=self.user1,receiver=self.user2)
+        self.assertAlmostEqual(message.creation_time,datetime.now(tz=timezone.utc),delta=timedelta(milliseconds=100))
