303491c92338c73219686ddc8045b8829dcc53a2
Mateo Tesorieri <70549780+Noxde@users.noreply.github.com>
2024-01-09 01:58:38 -0300

    Added a new context to manage the WebSocket The WebSocket will be available on all children of the layout component

---
+++ frontend/src/context/WebSocket.js
@@ -0,0 +1,72 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import AuthContext from "./AuthContext";
+
+const WebSocketContext = createContext();
+
+export default WebSocketContext;
+
+export const WebSocketProvider = ({ children }) => {
+  const [WebSocketS, setWebSocketS] = useState(null);
+  const [lastMessage, setLastMessage] = useState("");
+  const { authTokens } = useContext(AuthContext);
+
+  useEffect(() => {
+    const ws = new WebSocket(
+      "ws://localhost:8000/ws/chats/",
+      authTokens.access
+    );
+
+    ws.onopen = () => {
+      console.log("WebSocket connected.");
+      setWebSocketS(ws);
+    };
+    ws.onclose = () => {
+      console.log("WebSocket disconnected.");
+      setWebSocketS(null);
+    };
+    ws.onerror = (err) => {
+      console.log(err);
+    };
+    ws.onmessage = ({ data }) => {
+      setLastMessage(JSON.parse(data));
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  function close() {
+    if (WebSocketS) {
+      WebSocketS.close();
+      setWebSocketS(null);
+    }
+  }
+
+  /**
+   * Sends a message through the websocket connection
+   * @param {Number} dest Destination id
+   * @param {String} msg Message
+   */
+  function sendMessage(dest, msg) {
+    WebSocketS.send(
+      JSON.stringify({
+        content: msg,
+        receiver: dest,
+      })
+    );
+  }
+
+  return (
+    <WebSocketContext.Provider
+      value={{
+        ws: WebSocketS,
+        lastMessage,
+        close,
+        sendMessage,
+      }}
+    >
+      {children}
+    </WebSocketContext.Provider>
+  );
+};
