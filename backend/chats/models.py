8cd047d6800de28a20140639995123da5b316b9b
Valentino Amato <valentinoamatoroberts@gmail.com>
2024-02-08 14:17:54 -0300

    Update on_delete argument on Chat and Message models fields.
    Now Chats and  Messages won't be deleted along the owner user.

--- backend/chats/models.py
+++ backend/chats/models.py
@@ -3,16 +3,16 @@ from django.contrib.auth import get_user_model
 
 class Chat(models.Model): #Model for open chats, avoids searching in Message model
 
-    user_one = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,related_name="user_one")
-    user_two = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,related_name="user_two")
+    user_one = models.ForeignKey(get_user_model(), on_delete=models.DO_NOTHING, related_name="user_one", db_constraint=False)
+    user_two = models.ForeignKey(get_user_model(), on_delete=models.DO_NOTHING, related_name="user_two", db_constraint=False)
     show_to_user_one = models.BooleanField(default=True)
     show_to_user_two = models.BooleanField(default=True)
 
 class Message(models.Model): #Model for chat messages
 
     content = models.TextField(max_length=300)
-    sender = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,related_name="sender")
-    receiver = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,related_name="receiver")
+    sender = models.ForeignKey(get_user_model(), on_delete=models.DO_NOTHING, related_name="sender", db_constraint=False)
+    receiver = models.ForeignKey(get_user_model(), on_delete=models.DO_NOTHING, related_name="receiver", db_constraint=False)
     creation_time = models.DateTimeField(auto_now_add=True)
     show_to_sender = models.BooleanField(default=True)
     show_to_receiver = models.BooleanField(default=True)
