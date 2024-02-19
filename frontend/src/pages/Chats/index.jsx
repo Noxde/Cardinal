805932ef501591bc2212be9223174167799b80a0
Mateo Tesorieri <70549780+Noxde@users.noreply.github.com>
2024-02-19 19:59:12 -0300

    Moved the code for the chat to its own component All the effects, states and jsx was moved to a Chat component and the Chat component was imported here

--- frontend/src/pages/Chats/index.jsx
+++ frontend/src/pages/Chats/index.jsx
@@ -14,19 +14,16 @@ import NoChats from "./NoChats";
 import Followers from "./Followers";
 
 import { FiPlusCircle, FiArrowLeft } from "react-icons/fi";
+import Chat from "./Chat";
 
 function Chats() {
   const api = useAxios();
   const nav = useNavigate();
 
   const { authTokens, user } = useContext(AuthContext);
-  const { sendMessage, lastMessage } = useContext(WebSocketContext);
 
-  const chatRef = useRef();
   const [showFollowers, setShowFollowers] = useState(false);
-  const [openchat, setOpenChat] = useState(null);
-  const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState([]);
+  const [openChat, setOpenChat] = useState(null);
   const { width } = useWindowDimensions();
 
   useEffect(() => {
@@ -34,21 +31,6 @@ function Chats() {
       nav("/login/"); // If user is not logged in, redirect to login (should instead have "protected" routes)
     }
   }, [authTokens]);
-
-  // Reset messages
-  useEffect(() => {
-    setMessages([]);
-  }, [openchat]);
-
-  // Update messages
-  useEffect(() => {
-    setMessages((last) => [...last, lastMessage]);
-  }, [lastMessage]);
-
-  // Scroll whenever theres a new message
-  useEffect(() => {
-    chatRef.current?.scrollIntoView();
-  }, [messages]);
 
   const { data, isLoading, isError, error } = useQuery({
     queryKey: "openChats",
@@ -93,80 +75,13 @@ function Chats() {
           />
         )}
       </div>
-      <div id="openchat" className={`hidden md:block ${openchat && "!block"}`}>
-        {openchat ? (
-          <>
-            {/* Chat header */}
-            <div className="px-4 py-4 Gelion-Medium dark-blue border-b">
-              {/* Only shows on mobile, needed to backout from a chat */}
-              {width < 768 && (
-                <FiArrowLeft
-                  onClick={() => {
-                    setOpenChat(null);
-                  }}
-                  size={"20px"}
-                  className="mr-2 inline cursor-pointer"
-                />
-              )}
-              <img
-                src={openchat.chat_user.profileimg}
-                width={"41px"}
-                className="aspect-square rounded-full object-cover inline mr-4"
-                alt=""
-              />
-              {openchat.chat_user.username}
-            </div>
-            {/* Chat (could be a separate component) */}
-            {/* Chat messages */}
-            <div
-              id="messages"
-              className="flex flex-col overflow-scroll lg:h-[calc(100vh-(73px+45px))] p-4 md:h-[calc(100vh-15rem)] h-[calc(100vh-15rem)]"
-            >
-              {messages.map((x) => (
-                // #TODO: Message component with custom context menu
-                <div
-                  ref={chatRef}
-                  className={`message max-w-xs p-2 rounded-2xl ${
-                    x.receiver !== user.id
-                      ? "bg-[#4558ff] text-white rounded-br-none"
-                      : "bg-[hsl(234,100%,97%)] rounded-tl-none"
-                  }`}
-                  key={x.id}
-                  data-sender={x.receiver !== user.id ? "own" : "chat"}
-                >
-                  {x.content}
-                </div>
-              ))}
-            </div>
-            {/* Input */}
-            <div className="flex">
-              <input
-                onChange={(e) => {
-                  setMessage(e.target.value);
-                }}
-                value={message}
-                type="text"
-                name="message"
-                id="msg"
-                className="w-full"
-              />
-              <button
-                onClick={() => {
-                  sendMessage(openchat.chat_user.id, message);
-                  setMessages((last) => [
-                    ...last,
-                    {
-                      content: message,
-                      receiver: openchat.chat_user.id,
-                    },
-                  ]);
-                  setMessage("");
-                }}
-              >
-                Send
-              </button>
-            </div>
-          </>
+      <div id="openchat" className={`hidden md:block ${openChat && "!block"}`}>
+        {openChat ? (
+          <Chat
+            receiver={openChat.chat_user}
+            setOpenChat={setOpenChat}
+            key={openChat.chat_user.username}
+          />
         ) : (
           <div className="flex justify-center items-center h-full">
             <p className="text-xl Gelion-Medium text-center max-w-md dark-blue">
